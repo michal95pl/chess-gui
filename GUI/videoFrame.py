c968d141a996bf943f83cdea513b1eb4fc251092
Engarix <98103367+Engarix@users.noreply.github.com>
2025-09-12 00:05:19 +0200

    Update videoFrame.py
    resize

--- GUI/videoFrame.py
+++ GUI/videoFrame.py
@@ -1,12 +1,14 @@
+import os
 from threading import Thread, Lock
 from PIL import Image, ImageTk
 import tkinter as tk
 import cv2
 from image_processing.board_transformation import BoardTransformation
+from utils.logger import Logger
 
 class VideoFrame(Thread):
 
-    def __init__(self, root, camera_index: int, video_size=(800, 600), board_transformation: BoardTransformation = None):
+    def __init__(self, root, camera_index: int, video_size=(500, 500), board_transformation: BoardTransformation = None):
         super().__init__(daemon=True, name="VideoFrameThread")
 
         self.camera_index = camera_index
@@ -15,6 +17,7 @@ class VideoFrame(Thread):
         self.actual_frame = None
         self.board_transformation = board_transformation
         self.video_size = video_size
+        self.test_image_path = "assets/board_test.png"
 
         # create blank image for initialization
         blank_image = Image.new("RGB", (100, 100), color=(0, 0, 0))
@@ -35,27 +38,48 @@ class VideoFrame(Thread):
         self.update_video = False
 
     def run(self):
-        cap = cv2.VideoCapture(self.camera_index)
-        if not cap.isOpened():
-            print("Error: Could not open video.") # todo: add logs
-            return
+        if self.camera_index == -1:
+            if not os.path.exists(self.test_image_path):
+                Logger.log("Error: {test_image_path} not found.")
+                print("Error: {test_image_path} not found.")
+                return
+            frame = cv2.imread(self.test_image_path)
+            if frame is None:
+                Logger.log("Error: Could not read {test_image_path}.")
+                print("Error: Could not read {test_image_path}.")
+                return
 
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                print("Error: Could not read frame.") # todo: add logs
-                break
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = cv2.resize(frame, self.video_size) # todo: change size
+            frame = cv2.resize(frame, self.video_size)  # todo: change size
             if self.board_transformation is not None:
                 frame = self.board_transformation.transform(frame)
             self.actual_frame = frame
-
-            # it should be before show_frame function to make sure that thread stops immediately before showing new frame
-            if not self.update_video:
-                break
             self.show_frame(frame)
-        cap.release()
+
+        else:
+            cap = cv2.VideoCapture(self.camera_index)
+            if not cap.isOpened():
+                Logger.log("Error: Could not open video.")
+                print("Error: Could not open video.")
+                return
+
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    Logger.log("Error: Could not read frame.")
+                    print("Error: Could not read frame.")
+                    break
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame = cv2.resize(frame, self.video_size) # todo: change size
+                if self.board_transformation is not None:
+                    frame = self.board_transformation.transform(frame)
+                self.actual_frame = frame
+
+                # it should be before show_frame function to make sure that thread stops immediately before showing new frame
+                if not self.update_video:
+                    break
+                self.show_frame(frame)
+            cap.release()
 
     def __convert_frame_to_tk(self, frame):
         frame = Image.fromarray(frame)
